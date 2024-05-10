13c9ac5d71db3eb664c1b23849cb051cc455e5b3
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-10 22:39:57 +0530

    age checking for voting.

--- src/condition_state.java
+++ src/condition_state.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class condition_state {
     public static void main(String args[]){
+        Scanner call = new Scanner(System.in);
+         int age = call.nextInt();
+        //if condition check.
+        if(age >= 18){
+            System.out.println("congs! you can vote");
+        }else {
+            System.out.println("you're not eligible for vote");
+        }
 
     }
 }
