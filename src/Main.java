7cdde2ac86b02482a0435661b71acd4411e95986
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-10 18:23:46 +0530

    comment added

--- src/Main.java
+++ src/Main.java
@@ -19,5 +19,13 @@ public class Main {
        byte k =(byte)a;
        System.out.println(k);
 
+       byte x = 10;
+       byte y = 30;
+       int result = x * y;
+        System.out.println(result);
+
+        //assing opertor.
+
+
     }
 }
