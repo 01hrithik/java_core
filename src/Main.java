ad13e3120ddc1c50e78d96b8ea11d79fac4f5edd
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-10 12:42:05 +0530

    sum

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Hello World");
         }
+
+        System.out.println("sum of two value is : " +(9 + 1));
     }
 }
