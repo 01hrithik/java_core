2a7b5cde48b1aa4fab540a4d296f2a4723639aea
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-11 13:11:34 +0530

    added while loop

--- src/loop.java
+++ src/loop.java
@@ -1,2 +1,9 @@
 public class loop {
+    public static void main(String args[]){
+        int i =0;
+       while(i<4){
+           System.out.println("Hrithik Gorane"+i);
+           i++;
+       }
+    }
 }
