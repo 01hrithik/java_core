86868ac227c1379f1395fcd46a796a02dd880262
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-11 12:41:07 +0530

    if-else condition using getting highest number.

--- src/condition_state.java
+++ src/condition_state.java
@@ -11,10 +11,19 @@ public class condition_state {
         }
 
         //compare number.
+        System.out.print("enter first number :");
         int num_1=call.nextInt();
+        System.out.print("enter second number :");
         int num_2=call.nextInt();
+        System.out.print("enter third number :");
         int num_3=call.nextInt();
 
-
+        if((num_1 > num_2) && (num_1 > num_3)){
+            System.out.println("the highest num :"+num_1);
+        }else if((num_2>num_1)&&(num_2>num_3)){
+            System.out.println("the highest num :"+num_2);
+        }else{
+            System.out.println("the highest num :"+num_3);
+        }
     }
 }
