33f61d38daca85f4b5be92d6dcaf60424db97d4f
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-11 13:18:06 +0530

    do-while loop  using.

--- src/loop.java
+++ src/loop.java
@@ -2,8 +2,15 @@ public class loop {
     public static void main(String args[]){
         int i =0;
        while(i<4){
-           System.out.println("Hrithik Gorane"+i);
+           System.out.println("Hrithik Gorane");
            i++;
        }
+
+       //do-while loop use.
+        int a=0;
+        do{
+            System.out.println("happy");
+            a++;
+        }while(a<=4);
     }
 }
