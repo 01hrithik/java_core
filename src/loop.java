d2226a58edd68ad41abbc69eb077ccc2a33beaeb
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-11 13:20:21 +0530

    for loop added.

--- src/loop.java
+++ src/loop.java
@@ -1,7 +1,7 @@
 public class loop {
     public static void main(String args[]){
         int i =0;
-       while(i<4){
+       while(i<=4){
            System.out.println("Hrithik Gorane");
            i++;
        }
@@ -12,5 +12,10 @@ public class loop {
             System.out.println("happy");
             a++;
         }while(a<=4);
+
+        //for-loop using.
+        for(int num=0;num<=4; num++){
+            System.out.println("having good day!");
+        }
     }
 }
