87709c60a55c74c2a1ae5d8e23508eb3f28d1709
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-12 22:47:28 +0530

    added something

--- src/jagg.java
+++ src/jagg.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class jagg {
-    public static void main(String args){
+    public static void main(String args[]){
 
         //taking input from user.
         Scanner cs = new Scanner(System.in);
-        int[][] array = new int[2][3];
+        int[][] array = new int[2][5];
 
         for(int i=0; i< array.length; i++){
             for (int j=0; j<= array.length; j++){
+                array[i][j] = (int)(Math.random()*8);
                 System.out.print(array[i][j]+" ");
             }
             System.out.println();
