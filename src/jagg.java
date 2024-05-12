030205b0248944176731df7a26fc0df64c56fe7b
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-12 22:44:26 +0530

    for loop used to print jagged array

--- src/jagg.java
+++ src/jagg.java
@@ -3,5 +3,14 @@ public class jagg {
     public static void main(String args){
 
         //taking input from user.
+        Scanner cs = new Scanner(System.in);
+        int[][] array = new int[2][3];
+
+        for(int i=0; i< array.length; i++){
+            for (int j=0; j<= array.length; j++){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
