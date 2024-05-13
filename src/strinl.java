db3689f9c27a75d7c6a5d0cdfa1c2f93608a452c
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-13 09:45:49 +0530

    added something

--- src/strinl.java
+++ src/strinl.java
@@ -1,7 +1,10 @@
 public class strinl {
-    public static void main(String args){
-        String name = "Hrithik Gorane";
+    public static void main(String args[]){
+        String greeting = "Have a good day!";
         String Name = new String();
-
+        Name = "hey Hrithik Gorane!";
+        System.out.println(greeting);
+        System.out.println(Name);
+        System.out.println(Name.concat(" how are you?"));
     }
 }
