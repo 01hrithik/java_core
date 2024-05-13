b861a841e0c22e5b9a13e7cdaade85928275125e
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-13 16:02:02 +0530

    using string buffer fun

--- src/strinl.java
+++ src/strinl.java
@@ -6,5 +6,16 @@ public class strinl {
         System.out.println(greeting);
         System.out.println(Name);
         System.out.println(Name.concat(" how are you?"));
+        Name = Name+"good";
+
+        String name = "raj";
+        String name2 = "raj";
+        System.out.println(name2 == name);
+        System.out.println(name.compareTo(name2));
+        StringBuffer sbi = new StringBuffer("Hrithik");
+        System.out.println(sbi.capacity());
+        System.out.println("length of string is :"+sbi.length());
+        System.out.println("the append is :"+sbi.append(" love"));
+        System.out.println("the delete is :"+sbi.deleteCharAt(3));
     }
 }
