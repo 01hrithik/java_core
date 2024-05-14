f9aa41b52f6313335729f235215b33a12f9f204f
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-14 13:23:26 +0530

    reverse fun added.

--- src/revese.java
+++ src/revese.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class revese {
+        public static void main(String args[]){
+            Scanner call= new Scanner(System.in);
+            System.out.print("ENTER THE VALUE : ");
+            int Value = call.nextInt();
+            int revsev=0,reminder=0;
 
+            for(int i=0; i<Value; i++){
+                reminder= Value % 10;
+                revsev = revsev * 10 + reminder;
+                Value /= 10;
+            }
+            System.out.println(revsev);
+        }
 }
