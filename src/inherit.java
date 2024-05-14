d92eb9f80986a3245dff8a778d1ed55bb5716cc3
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-14 14:55:13 +0530

    inhrit class

--- src/inherit.java
+++ src/inherit.java
@@ -1,2 +1,27 @@
+class eddie{
+    public eddie(){
+        super();
+        System.out.println("hello there!");
+    }
+    public eddie(int n){
+        super();
+        System.out.println("in eddie phone message pop-up");
+    }
+}
+
+class meddie extends eddie{
+    public meddie(){
+        super();
+        System.out.println("in mddie");
+    }
+    public meddie(int n){
+        this();
+        System.out.println("in meddie messeg pop up");
+    }
+}
+
 public class inherit {
+    public static void main(String args[]){
+       meddie phone = new meddie(5);
+    }
 }
