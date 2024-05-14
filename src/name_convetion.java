50ac0bdd08c72fb1d4b126618c1d773606cc074b
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-14 15:04:10 +0530

    added something.

--- src/name_convetion.java
+++ src/name_convetion.java
@@ -1,5 +1,12 @@
 public class name_convetion {
     public static void main (String args[]){
 
+        /*
+        class convetion naming :
+           class naming example-> Animal,Good
+           variable & method naming example -> mark,human,love()
+           constant naming examole-> PIE,BRAND
+           two letter for naming example -> showMyMark()
+         */
     }
 }
