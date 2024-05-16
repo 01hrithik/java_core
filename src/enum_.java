38577ae64bd2e36ee6d4467a0eaf34d848c3c242
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-16 12:10:19 +0530

    enum use  status print values of varraiblr.

--- src/enum_.java
+++ src/enum_.java
@@ -1,2 +1,14 @@
+import java.util.Arrays;
+
+enum Status{
+    Run,Rest,Pending,Success;
+}
 public class enum_ {
+    public static void main(String[] args) {
+
+        Status[] s = Status.values();
+        System.out.println(Arrays.toString(s));
+
+
+    }
 }
