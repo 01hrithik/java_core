c23896809dc4b360c4fb4290b40faf6259916813
Hrithik Gorane <hrithikgorane@gmail.com>
2024-05-17 13:36:51 +0530

    theresds setting.

--- src/th.java
+++ src/th.java
@@ -1,5 +1,31 @@
+class Hate extends Thread{
+    public void run(){
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("i hate you"+" "+i);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
+class LOVE extends Thread{
+    public void run(){
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("i love you"+" "+i);
+        }
+    }
+}
 public class th {
     public static void main(String[] args) {
+ Hate iol =  new Hate();
+ LOVE loi = new LOVE();
+        System.out.println(iol.getPriority());
+        loi.setPriority(Thread.MAX_PRIORITY);
+ iol.start();
+ loi.start();
 
     }
 }
